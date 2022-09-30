53df5534cc046e8b0e15e36e1ef8d79de9107a27
Kramer Brown <brownkramer63@gmail.com>
2022-09-30 10:37:45 -0500

    ticketprojectday01

---
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -0,0 +1,30 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.RoleDTO;
+import com.cydeo.dto.UserDTO;
+import com.cydeo.entity.User;
+import com.cydeo.service.UserService;
+
+import java.util.List;
+
+public class UserServiceImpl extends AbstractMapService<UserDTO, String> implements UserService {
+    @Override
+    public UserDTO save(UserDTO user) {
+        return null;
+    }
+
+    @Override
+    public UserDTO findById(String username) {
+        return null;
+    }
+
+    @Override
+    public List<UserDTO> findAll() {
+        return null;
+    }
+
+    @Override
+    public void deleteById(String username) {
+
+    }
+}
