d1c7bcdf0c0c24fdeac37ae82e5b7333f4a3ff1a
Kramer Brown <brownkramer63@gmail.com>
2022-10-02 15:04:40 -0500

    ticketprojectday03

--- src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -3,33 +3,37 @@ package com.cydeo.service.impl;
 import com.cydeo.dto.ProjectDTO;
 import com.cydeo.service.CrudService;
 import com.cydeo.service.ProjectService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public class ProjectServiceImpl extends AbstractMapService<ProjectDTO, String> implements CrudService {
+@Service
+public class ProjectServiceImpl extends AbstractMapService<ProjectDTO, String> implements ProjectService {
+
+
 
     @Override
-    public Object save(Object user) {
-        return null;
+    public ProjectDTO save(ProjectDTO project) {
+        return super.save(project.getProjectCode(),project);
     }
 
     @Override
-    public Object findById(Object username) {
-        return null;
+    public ProjectDTO findById(String projectCode) {
+        return super.findById(projectCode);
     }
 
     @Override
-    public List findAll() {
-        return null;
+    public List<ProjectDTO> findAll() {
+        return  super.findAll();
     }
 
     @Override
-    public void deleteById(Object username) {
-
+    public void update(ProjectDTO object) {
+super.update(object.getProjectCode(),object);
     }
 
     @Override
-    public void update(Object object) {
-
+    public void deleteById(String projectCode) {
+super.deleteById(projectCode);
     }
 }
